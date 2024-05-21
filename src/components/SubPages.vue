9d4bb6c88d660f4b4cb02cbca88342d9f174d4df
Fred <fred@192.168.1.130>
2024-05-21 15:05:56 +0100

    QA edits

--- src/components/SubPages.vue
+++ src/components/SubPages.vue
@@ -1,83 +1,66 @@
 <template>
-    <div>
-        <div class="border-t-16 border-[#C0F659] w-full"></div>
-        <div class="bg-white pt-0 px-4 md:px-16">
-            <div class="flex flex-col md:flex-row items-start mt-8 px-4 md:px-16 space-y-8 md:space-y-0 md:space-x-8">
-                <div class="md:w-1/2 flex justify-center md:justify-start">
-                    <img src="@/assets/Teamarrow.png" alt="Sample Image" class="w-full md:w-[68%] h-auto" />
-                </div>
-                <div class="md:w-1/2 flex flex-col justify-between">
-                    <div>
-                        <h3 class="text-left text-3xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-[#1E3036]">
-                            Your Partner in <span class="text-[#BCE272]">HR</span><br />
-                            <span class="block mt-2"><span class="text-[#BCE272]">Recruitment</span> Success</span>
-                        </h3>
-                        <p class="mt-8 text-left text-[#1E3036]">
-                            Ut fugit prehento endelent dolest rem rerati dolorrovid qui optae voluptios molorro ribus,
-                            ut
-                            quaepro earciis volorum quos illorer iorerroreped quide sin essum faccullor sa volore
-                            nonsedio
-                            ipsa dolorior si ut maximin non reiciis quistibus rem con pero dolupta tquodia idi velendi
-                            genditio. Igende illoremporem fugia conet vollitatur sed quamustiur?
-                        </p>
-                    </div>
-                    <button
-                        class="mt-8 md:mt-12 bg-white border border-[#C0F659] text-black py-2 px-4 rounded-full w-[162px] hover:bg-[#C0F659] hover:text-white transition duration-300">
-                        Find Out More
-                    </button>
-                </div>
-            </div>
-            <div
-                class="mt-20 flex flex-col pb-24 md:flex-row justify-between space-y-4 md:space-y-0 md:space-x-4 px-4 md:px-16">
-                <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(1)">
-                    <img src="@/assets/Rectangle1.png" alt="Image 1" class="w-full h-full object-cover" />
-                    <h4
-                        class="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold group-hover:text-[#C0F659] transition duration-300">
-                        Services</h4>
-                    <img src="@/assets/Arrowvector1.png" alt="Arrow Icon"
-                        class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
-                    <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover"
-                        class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
-                </div>
-                <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(2)">
-                    <img src="@/assets/Rectangle2.png" alt="Image 2" class="w-full h-full object-cover" />
-                    <h4
-                        class="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold group-hover:text-[#C0F659] transition duration-300">
-                        About Us</h4>
-                    <img src="@/assets/Arrowvector1.png" alt="Arrow Icon"
-                        class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
-                    <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover"
-                        class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
-                </div>
-                <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(3)">
-                    <img src="@/assets/Rectangle3.png" alt="Image 3" class="w-full h-full object-cover" />
-                    <h4
-                        class="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold group-hover:text-[#C0F659] transition duration-300">
-                        Contact</h4>
-                    <img src="@/assets/Arrowvector1.png" alt="Arrow Icon"
-                        class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
-                    <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover"
-                        class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
-                </div>
-            </div>
+  <div>
+    <div class="border-t-16 border-[#C0F659] w-full"></div>
+    <div class="bg-white pt-0 px-4 md:px-16">
+      <div class="flex flex-col md:flex-row items-start mt-8 px-4 md:px-16 space-y-8 md:space-y-0 md:space-x-8">
+        <div class="md:w-1/2 flex justify-center md:justify-start">
+          <img src="@/assets/Teamarrow.png" alt="Sample Image" class="w-full md:w-[68%] h-auto" />
         </div>
+        <div class="md:w-1/2 flex flex-col justify-between">
+          <div>
+            <h3 class="mt-8 text-left text-3xl md:text-4xl lg:text-5xl font-medium text-[#1E3036] font-clash-display-medium">
+              Your Partner in <span class="text-[#BCE272]">HR</span><br />
+              <span class="block mt-2"><span class="text-[#BCE272]">Recruitment</span> Success</span>
+            </h3>
+            <p class="mt-8 text-left text-[#1E3036] font-clash-display-regular">
+              Ut fugit prehento endelent dolest rem rerati dolorrovid qui optae voluptios molorro ribus, ut quaepro earciis volorum quos illorer iorerroreped quide sin essum faccullor sa volore nonsedio ipsa dolorior si ut maximin non reiciis quistibus rem con pero dolupta tquodia idi velendi genditio. Igende illoremporem fugia conet vollitatur sed quamustiur?
+            </p>
+          </div>
+          <button class="mt-8 mb-12 md:mt-20 lg:mt-24 bg-white border border-[#C0F659] text-black py-2 px-4 rounded-full w-[162px] hover:bg-[#C0F659] hover:text-white transition duration-300 md:mb-0">
+            Find Out More
+          </button>
+        </div>
+      </div>
+      <div class="hidden md:flex mt-20 pb-24 justify-between space-y-4 md:space-y-0 md:space-x-4 px-4 md:px-16">
+        <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(1)">
+          <div class="w-full h-full bg-cover bg-center transition duration-300" :style="{ backgroundImage: `url(${require('@/assets/Rectangle1.png')})` }"></div>
+          <div class="absolute inset-0 bg-[#283C43] opacity-0 group-hover:opacity-100 transition duration-300"></div>
+          <h4 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold group-hover:text-[#C0F659] transition duration-300">Services</h4>
+          <img src="@/assets/Arrowvector1.png" alt="Arrow Icon" class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
+          <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover" class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
+        </div>
+        <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(2)">
+          <div class="w-full h-full bg-cover bg-center transition duration-300" :style="{ backgroundImage: `url(${require('@/assets/Rectangle2.png')})` }"></div>
+          <div class="absolute inset-0 bg-[#283C43] opacity-0 group-hover:opacity-100 transition duration-300"></div>
+          <h4 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold group-hover:text-[#C0F659] transition duration-300">About Us</h4>
+          <img src="@/assets/Arrowvector1.png" alt="Arrow Icon" class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
+          <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover" class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
+        </div>
+        <div class="relative flex-1 h-[416px] cursor-pointer group" @click="onClickRectangle(3)">
+          <div class="w-full h-full bg-cover bg-center transition duration-300" :style="{ backgroundImage: `url(${require('@/assets/Rectangle3.png')})` }"></div>
+          <div class="absolute inset-0 bg-[#283C43] opacity-0 group-hover:opacity-100 transition duration-300"></div>
+          <h4 class="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold group-hover:text-[#C0F659] transition duration-300">Contact</h4>
+          <img src="@/assets/Arrowvector1.png" alt="Arrow Icon" class="absolute top-4 right-4 w-8 h-8 group-hover:hidden">
+          <img src="@/assets/Arrowvector2.png" alt="Arrow Icon Hover" class="absolute top-4 right-4 w-8 h-8 hidden group-hover:block">
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'SubPages',
-    methods: {
-        onClickRectangle(rectangleId) {
-            alert(`Rectangle ${rectangleId} clicked`);
-            
-        }
+  name: 'SubPages',
+  methods: {
+    onClickRectangle(rectangleId) {
+      alert(`Rectangle ${rectangleId} clicked`);
     }
+  }
 }
 </script>
 
 <style scoped>
 .border-t-16 {
-    border-top-width: 16px;
+  border-top-width: 16px;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"clash-display": ["ClashDisplay-Regular", "sans-serif"],
+				"clash-display-extralight": ["ClashDisplay-Extralight", "sans-serif"],
+				"clash-display-light": ["ClashDisplay-Light", "sans-serif"],
+				"clash-display-medium": ["ClashDisplay-Medium", "sans-serif"],
+				"clash-display-semibold": ["ClashDisplay-Semibold", "sans-serif"],
+				"clash-display-bold": ["ClashDisplay-Bold", "sans-serif"],
+				"clash-display-variable": ["ClashDisplay-Variable", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
